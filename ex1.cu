61694f3c01fe01458c7422ee46e8c7102aee3209
Your Name <noa.shamir@campus.technion.ac.il>
2026-05-21 13:37:14 +0300

    Question 4 NIGMARR

--- ex1.cu
+++ ex1.cu
@@ -28,6 +28,10 @@ __device__ void prefix_sum(int arr[], int arr_size) {
 __device__ void build_histogram(int hist[], uchar all_in[IMG_HEIGHT][IMG_WIDTH], 
                                 int tile_start_pixel_row, int tile_start_pixel_col){
     int tid = threadIdx.x;
+
+    hist[tid] = 0;
+    __syncthreads();
+
     int row;
     int col;
     for(int stride = 0; stride < TILE_WIDTH * TILE_WIDTH; stride += NUM_THREADS){
@@ -69,9 +73,6 @@ __global__ void process_image_kernel(uchar *all_in, uchar *all_out, uchar *maps)
     
     for (int tile_idx = 0; tile_idx < TILE_COUNT*TILE_COUNT; tile_idx++){
 
-        hist[threadIdx.x] = 0;
-        __syncthreads();
-
         tile_row = tile_idx / TILE_COUNT;
         tile_col = tile_idx % TILE_COUNT;
         tile_start_pixel_row = tile_row * TILE_WIDTH;
@@ -103,9 +104,9 @@ struct task_serial_context *task_serial_init()
 {
     auto context = new task_serial_context;
 
-    cudaMalloc((void**) &context->d_in_img, IMG_SIZE);
-    cudaMalloc((void**) &context->d_out_img, IMG_SIZE);
-    cudaMalloc((void**) &context->d_maps, TILE_COUNT * TILE_COUNT * 256);
+    CUDA_CHECK(cudaMalloc((void**) &context->d_in_img, IMG_SIZE));
+    CUDA_CHECK(cudaMalloc((void**) &context->d_out_img, IMG_SIZE));
+    CUDA_CHECK(cudaMalloc((void**) &context->d_maps, TILE_COUNT * TILE_COUNT * 256));
 
     return context;
 }
@@ -118,22 +119,16 @@ void task_serial_process(struct task_serial_context *context, uchar *images_in, 
     for(int i = 0; i < N_IMAGES; i++){
 
         // 1. copy the relevant image from images_in to the GPU memory you allocated
-        cudaMemcpy(context->d_in_img, images_in + i * IMG_SIZE, IMG_SIZE, cudaMemcpyHostToDevice);
+        CUDA_CHECK(cudaMemcpy(context->d_in_img, images_in + i * IMG_SIZE, IMG_SIZE, cudaMemcpyHostToDevice));
 
         // 2. invoke GPU kernel on this image
         process_image_kernel<<<1, NUM_THREADS>>>(context->d_in_img, context->d_out_img, context->d_maps);
 
-        cudaDeviceSynchronize();
+        CUDA_CHECK(cudaDeviceSynchronize());
 
-        // 2.5. get error
-        cudaError_t error = cudaGetLastError();
-        if (error!=cudaSuccess){
-            fprintf(stderr, "Kernel execution failed:%s\n", cudaGetErrorString(error));
-            return;
-        }
-
+    
         // 3. copy output from GPU memory to relevant location in images_out_gpu_serial
-        cudaMemcpy(images_out + i * IMG_SIZE, context->d_out_img, IMG_SIZE, cudaMemcpyDeviceToHost);
+        CUDA_CHECK(cudaMemcpy(images_out + i * IMG_SIZE, context->d_out_img, IMG_SIZE, cudaMemcpyDeviceToHost));
 
     }
 
@@ -143,16 +138,57 @@ void task_serial_process(struct task_serial_context *context, uchar *images_in, 
 void task_serial_free(struct task_serial_context *context)
 {
     //free resources allocated in task_serial_init
-    cudaFree(context->d_in_img);
-    cudaFree(context->d_out_img);
-    cudaFree(context->d_maps);
+    CUDA_CHECK(cudaFree(context->d_in_img));
+    CUDA_CHECK(cudaFree(context->d_out_img));
+    CUDA_CHECK(cudaFree(context->d_maps));
 
     free(context);
 }
 
+//===============================================================================
+//                           BULK GPU IMPLEMENTATION
+//===============================================================================
+
+__global__ void process_multiple_image_kernel(uchar *all_in, uchar *all_out, uchar *maps) {
+    
+    int img_idx = blockIdx.x;
+
+    all_in  += img_idx * IMG_SIZE;
+    all_out += img_idx * IMG_SIZE;
+    maps    += img_idx * (TILE_COUNT * TILE_COUNT * 256);
+    
+    __shared__ int hist[256]; //shared for atomic add
+    int *CDF;
+
+    int tile_row;
+    int tile_col;
+    int tile_start_pixel_row;
+    int tile_start_pixel_col;
+    
+    for (int tile_idx = 0; tile_idx < TILE_COUNT*TILE_COUNT; tile_idx++){
+
+        tile_row = tile_idx / TILE_COUNT;
+        tile_col = tile_idx % TILE_COUNT;
+        tile_start_pixel_row = tile_row * TILE_WIDTH;
+        tile_start_pixel_col = tile_col * TILE_WIDTH;
+        
+        build_histogram(hist, (uchar (*)[IMG_WIDTH])all_in, tile_start_pixel_row, tile_start_pixel_col);
+        CDF = hist;
+        prefix_sum(CDF, 256);
+
+        calc_m_v((uchar (*)[TILE_COUNT][256])maps, CDF, tile_row, tile_col);
+
+    }
+    
+    interpolate_device(maps, all_in, all_out);
+    return; 
+}
+
 /* Bulk GPU context struct with necessary CPU / GPU pointers to process all the images */
 struct gpu_bulk_context {
-    // TODO define bulk-GPU memory buffers
+    uchar *d_all_in;
+    uchar *d_all_out;
+    uchar *d_maps;
 };
 
 /* Allocate GPU memory for all the input images, output images, and maps.
@@ -162,7 +198,9 @@ struct gpu_bulk_context *gpu_bulk_init()
 {
     auto context = new gpu_bulk_context;
 
-    //TODO: allocate GPU memory for all the input images, output images, and maps
+        CUDA_CHECK(cudaMalloc((void**) &context->d_all_in, N_IMAGES * IMG_SIZE));
+        CUDA_CHECK(cudaMalloc((void**) &context->d_all_out, N_IMAGES * IMG_SIZE));
+        CUDA_CHECK(cudaMalloc((void**) &context->d_maps, N_IMAGES * (TILE_COUNT * TILE_COUNT * 256)));
 
     return context;
 }
@@ -171,15 +209,22 @@ struct gpu_bulk_context *gpu_bulk_init()
  * provided output host array */
 void gpu_bulk_process(struct gpu_bulk_context *context, uchar *images_in, uchar *images_out)
 {
-    //TODO: copy all input images from images_in to the GPU memory you allocated
-    //TODO: invoke a kernel with N_IMAGES threadblocks, each working on a different image
-    //TODO: copy output images from GPU memory to images_out
+    // 1. copy all input images from images_in to the GPU memory you allocated
+    CUDA_CHECK(cudaMemcpy(context->d_all_in, images_in, N_IMAGES * IMG_SIZE, cudaMemcpyHostToDevice));
+    // 2. invoke a kernel with N_IMAGES threadblocks, each working on a different image
+    process_multiple_image_kernel<<<N_IMAGES, NUM_THREADS>>>(context->d_all_in, context->d_all_out, context->d_maps);
+    CUDA_CHECK(cudaDeviceSynchronize());
+    // 3. copy output images from GPU memory to images_out
+    CUDA_CHECK(cudaMemcpy(images_out, context->d_all_out, N_IMAGES * IMG_SIZE, cudaMemcpyDeviceToHost));
 }
 
 /* Release allocated resources for the bulk GPU implementation. */
 void gpu_bulk_free(struct gpu_bulk_context *context)
 {
-    //TODO: free resources allocated in gpu_bulk_init
+    // 1. free GPU memory allocated in gpu_bulk_init
+    CUDA_CHECK(cudaFree(context->d_all_in));
+    CUDA_CHECK(cudaFree(context->d_all_out));
+    CUDA_CHECK(cudaFree(context->d_maps));
 
     free(context);
 }
